e576f4d4cc3957f938663b165ac690e72e7fe382
Doxstart <127298109+Doxstart@users.noreply.github.com>
2023-04-19 12:58:50 +0200

    lez. 19/04/2023

--- services/data-service.js
+++ services/data-service.js
@@ -1,12 +1,14 @@
 class DataService{
 
-    static getSeries(){
+    static DATA_URL = 'https://6437a2bd0c58d3b145753906.mockapi.io/Series';
 
+    static getSeries(){
+        return fetch(this.DATA_URL).then(resp => resp.json())
     }
 
     static putSerie(serie){
-
+        return fetch(this.DATA_URL + '/' + serie.id, {method: 'PUT', headers: {'content-type': 'application/json'}, body: JSON.stringify(serie) })
+        .then(resp => resp.json());
     }
-
 
 }
